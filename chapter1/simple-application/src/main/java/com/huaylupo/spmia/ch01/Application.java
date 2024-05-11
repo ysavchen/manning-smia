a596c7fe920ada98a0a3b1eaf7c424225bb316b6
Yuriy Savchenko <ysavchen@outlook.com>
2024-05-12 01:52:51 +0300

    Formatting

--- chapter1/simple-application/src/main/java/com/huaylupo/spmia/ch01/Application.java
+++ chapter1/simple-application/src/main/java/com/huaylupo/spmia/ch01/Application.java
@@ -12,42 +12,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(value="hello")
+@RequestMapping(value = "hello")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@GetMapping(value="/{firstName}")
-	public String helloGET( 
-			@PathVariable("firstName") String firstName,
-			@RequestParam("lastName") String lastName) {
-		return String.format("{\"message\":\"Hello %s %s\"}",firstName, lastName);
-	}
-	
-	@PostMapping
-	public String helloPOST( @RequestBody HelloRequest request) {
-		return String.format("{\"message\":\"Hello %s %s\"}",request.getFirstName(), request.getLastName());
-	}
+    @GetMapping(value = "/{firstName}")
+    public String helloGET(
+            @PathVariable("firstName") String firstName,
+            @RequestParam("lastName") String lastName) {
+        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+    }
+
+    @PostMapping
+    public String helloPOST(@RequestBody HelloRequest request) {
+        return String.format("{\"message\":\"Hello %s %s\"}", request.getFirstName(), request.getLastName());
+    }
 }
 
-class HelloRequest{
-	
-	private String firstName;
-	private String lastName;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+class HelloRequest {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
 
